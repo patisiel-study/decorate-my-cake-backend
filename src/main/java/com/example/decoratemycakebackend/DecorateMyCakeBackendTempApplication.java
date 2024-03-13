a8ec1052ff1ba7b44647ae67814a7414cdaee95c
lazarus0320 <astar5327z@gmail.com>
2024-03-13 13:51:21 +0900

    feat: 프로젝트 구축

---
+++ src/main/java/com/example/decoratemycakebackend/DecorateMyCakeBackendTempApplication.java
@@ -0,0 +1,13 @@
+package com.example.decoratemycakebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecorateMyCakeBackendTempApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecorateMyCakeBackendTempApplication.class, args);
+    }
+
+}
