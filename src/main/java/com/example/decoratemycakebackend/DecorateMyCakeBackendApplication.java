de223b6d72c54305143488ff63d9f95971c29366
lazarus0320 <astar5327z@gmail.com>
2024-03-24 19:14:08 +0900

    rename: 프로젝트명 변경

---
+++ src/main/java/com/example/decoratemycakebackend/DecorateMyCakeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.decoratemycakebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecorateMyCakeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecorateMyCakeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/decoratemycakebackend/DecorateMyCakeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.decoratemycakebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecorateMyCakeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
