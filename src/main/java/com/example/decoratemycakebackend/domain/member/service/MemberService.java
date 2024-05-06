126e4241a54728cce03c8649aac951076a96b4dc
larzarus0320 <astar5327z@gmail.com>
2024-05-06 11:03:23 +0900

    fix: 로그인 예외처리 버그 수정

--- src/main/java/com/example/decoratemycakebackend/domain/member/service/MemberService.java
+++ src/main/java/com/example/decoratemycakebackend/domain/member/service/MemberService.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -40,11 +41,16 @@ public class MemberService {
     // 2. 실제 검증. authenticate() 메서드를 통해 요청된 Member에 대한 검증 진행
     // authenticate 메서드가 실행될 때 CustomUserDetailsService에서 만든 loadUserByUsername 메서드 실행
 
-        Authentication authentication = authenticationManagerBuilder
-                .getObject().authenticate(authenticationToken);
+        try {
+            Authentication authentication = authenticationManagerBuilder
+                    .getObject().authenticate(authenticationToken);
 
-        // 3. 인증 정보를 기반으로 JWT 토큰 생성
-        return jwtTokenProvider.generateToken(authentication);
+            return jwtTokenProvider.generateToken(authentication);
+        } catch (AuthenticationException e) {
+            throw new CustomException(ErrorCode.LOGIN_FAILURE);
+        } catch (Exception e) {
+            throw new CustomException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
 
     }
 
