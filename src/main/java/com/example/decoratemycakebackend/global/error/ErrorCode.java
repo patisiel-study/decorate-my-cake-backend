020267dba8e81a705bede1a0027e1d3ad3659f23
larzarus0320 <astar5327z@gmail.com>
2024-05-11 23:46:34 +0900

    test: 케이크 생성 금지 에러 추가

--- src/main/java/com/example/decoratemycakebackend/global/error/ErrorCode.java
+++ src/main/java/com/example/decoratemycakebackend/global/error/ErrorCode.java
@@ -40,8 +40,8 @@ public enum ErrorCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 회원을 찾을 수 없습니다."),
     CAKE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 케이크를 찾을 수 없습니다."),
     CANDLE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 캔들을 찾을 수 없습니다."),
+    FORBIDDEN_CREATE_CAKE(HttpStatus.OK, "D-30일부터 케이크를 생성할 수 있습니다."),
     FRIEND_REQUEST_NOT_FOUND(HttpStatus.NOT_FOUND, "친구 요청을 찾을 수 없습니다.");
-
 
 
     private final HttpStatus status;
