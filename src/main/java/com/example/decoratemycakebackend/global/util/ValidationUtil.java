b2842b06e5e56ceceeeee719e2cf0d3e8a852baf
larzarus0320 <astar5327z@gmail.com>
2024-05-11 23:45:50 +0900

    fix: 로그인 회원 검증 로직 변경

--- src/main/java/com/example/decoratemycakebackend/global/util/ValidationUtil.java
+++ src/main/java/com/example/decoratemycakebackend/global/util/ValidationUtil.java
@@ -2,10 +2,14 @@ package com.example.decoratemycakebackend.global.util;
 
 import com.example.decoratemycakebackend.global.error.CustomException;
 import com.example.decoratemycakebackend.global.error.ErrorCode;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class ValidationUtil {
-    public static void validateEmailMatch(String email, String requestEmail) {
-        if (!email.equals(requestEmail)) {
+
+    public static void validateCurrentEmail(String email) {
+        // 지금 시큐리티에 올라가있는 이메일과 같은지 확인
+        if (!email.equals(SecurityUtil.getCurrentUserEmail())) {
             throw new CustomException(ErrorCode.ACCOUNT_NOT_MATCHED);
         }
     }
