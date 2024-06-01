7a92e589ab0bc200ed4b9242c8428149dec9a476
larzarus0320 <astar5327z@gmail.com>
2024-06-01 20:46:31 +0900

    feat: 로그아웃 및 RT 재발급 기능 구현(redis)

--- src/main/java/com/example/decoratemycakebackend/domain/member/controler/MemberController.java
+++ src/main/java/com/example/decoratemycakebackend/domain/member/controler/MemberController.java
@@ -2,9 +2,11 @@ package com.example.decoratemycakebackend.domain.member.controler;
 
 import com.example.decoratemycakebackend.domain.member.dto.LogInDto;
 import com.example.decoratemycakebackend.domain.member.dto.MemberDto;
+import com.example.decoratemycakebackend.domain.member.dto.RefreshTokenRequest;
 import com.example.decoratemycakebackend.domain.member.dto.SignUpDto;
 import com.example.decoratemycakebackend.domain.member.service.MemberService;
 import com.example.decoratemycakebackend.global.auth.JwtToken;
+import com.example.decoratemycakebackend.global.auth.JwtTokenProvider;
 import com.example.decoratemycakebackend.global.util.ResponseDto;
 import com.example.decoratemycakebackend.global.util.SecurityUtil;
 import io.swagger.v3.oas.annotations.Operation;
@@ -27,6 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/member")
 public class MemberController {
     private final MemberService memberService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Operation(summary = "로그인", description = "이메일과 비밀번호를 사용하여 로그인합니다.")
     @ApiResponses(value = {
@@ -65,5 +68,24 @@ public class MemberController {
     @PostMapping("/test")
     public String test() {
         return SecurityUtil.getCurrentUserEmail();
+    }
+
+    @Operation(summary = "Refresh Token 재발급", description = "Access Token 만료시 기존 Refresh Token을 이쪽으로 보내서 새로운 Access Token 받아가기")
+    @PostMapping("/refresh")
+    public ResponseEntity<ResponseDto<?>> refreshToken(@RequestBody RefreshTokenRequest refreshTokenRequest) {
+        JwtToken token = jwtTokenProvider.refreshAccessToken(refreshTokenRequest.getRefreshToken());
+        return ResponseEntity.ok(new ResponseDto<>("Access Token 재발급 완료.", token));
+    }
+
+    @Operation(summary = "로그아웃", description = "현재 로그인 된 계정의 로그아웃")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "로그아웃 성공"),
+            @ApiResponse(responseCode = "401", description = "인증 실패")
+    })
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout() {
+        String email = SecurityUtil.getCurrentUserEmail();
+        memberService.logout(email);
+        return ResponseEntity.ok(new ResponseDto<>("로그아웃 되었습니다ㅏ.", null));
     }
 }
