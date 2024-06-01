99d711ce9b9d947a930ad71f8ee8bd72912f89dc
larzarus0320 <astar5327z@gmail.com>
2024-06-01 18:40:53 +0900

    fix: 로그인 검증 기능 수정

--- src/main/java/com/example/decoratemycakebackend/global/util/ValidationUtil.java
+++ src/main/java/com/example/decoratemycakebackend/global/util/ValidationUtil.java
@@ -4,8 +4,9 @@ import com.example.decoratemycakebackend.global.error.CustomException;
 import com.example.decoratemycakebackend.global.error.ErrorCode;
 
 public class ValidationUtil {
-    public static void validateEmailMatch(String email, String requestEmail) {
-        if (!email.equals(requestEmail)) {
+    public static void validateCurrentEmail(String email) {
+        // 지금 시큐리티에 올라가있는 이메일과 같은지 확인
+        if (!email.equals(SecurityUtil.getCurrentUserEmail())) {
             throw new CustomException(ErrorCode.ACCOUNT_NOT_MATCHED);
         }
     }
