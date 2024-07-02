9a6e96735a0802e1426079b55bca7769dca4e66a
larzarus0320 <astar5327z@gmail.com>
2024-07-02 12:10:19 +0900

    Fix: 케이크 설정 변경시 이미지도 변경 가능하도록 수정

--- src/main/java/com/example/decoratemycakebackend/domain/candle/dto/CandleGetByCakeRequestDto.java
+++ src/main/java/com/example/decoratemycakebackend/domain/candle/dto/CandleGetByCakeRequestDto.java
@@ -7,7 +7,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class CandleGetRequestDto {
+public class CandleGetByCakeRequestDto {
     private String email;
     private Integer cakeCreatedYear;
 
---
+++ src/main/java/com/example/decoratemycakebackend/domain/candle/entity/CandleName.java
@@ -0,0 +1,5 @@
+package com.example.decoratemycakebackend.domain.candle.entity;
+
+public enum CandleName {
+    GREEN_CANDLE, PINK_CANDLE
+}
